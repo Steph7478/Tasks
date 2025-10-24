21464ae141600e61b4450d4d7ca8cd9fca29b0d4
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-24 09:17:25 -0300

    feat: program.cs configs

--- Program.cs
+++ Program.cs
@@ -1,41 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Configuration;
+using Tasks.Infrastructure.Config;
+using DotNetEnv;
+using Tasks.Infrastructure.Context;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "dev";
 
-var app = builder.Build();
+Env.Load($".env.{environment}");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables();
 
-app.UseHttpsRedirection();
+var config = builder.Build();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+bool useInMemory = environment == "test";
+var options = DatabaseConfig.GetDbOptions(config, useInMemory);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+using var context = new AppDbContext(options);
+context.Database.EnsureCreated();
