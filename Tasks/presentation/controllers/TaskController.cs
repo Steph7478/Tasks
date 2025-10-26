20d50c0db754745599e630b139991ca246c08686
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-26 10:53:03 -0300

    feat: Task Controller

---
+++ Tasks/presentation/controllers/TaskController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Tasks.Application.Usecases;
+using Tasks.Presentation.DTOs;
+using Tasks.Presentation.Mappers;
+
+namespace Tasks.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController(AddTask addTaskUseCase, GetTaskById getTaskByIdUseCase) : ControllerBase
+{
+    private readonly AddTask _addTaskUseCase = addTaskUseCase;
+    private readonly GetTaskById _getTaskByIdUseCase = getTaskByIdUseCase;
+
+    [HttpPost("add")]
+    public async Task<IActionResult> CreateTask([FromBody] TaskRequest request)
+    {
+
+        var appRequest = TaskPresentationMapper.ToApplication(request);
+        var appResponse = await _addTaskUseCase.ExecuteAsync(appRequest);
+        var response = TaskPresentationMapper.ToApi(appResponse);
+
+        return Ok(response);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetTaskById(Guid id)
+    {
+        var appResponse = await _getTaskByIdUseCase.ExecuteAsync(id);
+        if (appResponse == null) return NotFound();
+        var response = TaskPresentationMapper.ToApi(appResponse);
+
+        return Ok(response);
+    }
+}
