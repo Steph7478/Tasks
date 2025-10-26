babe800118def791a2a7dc5ac0a42c7efd71741b
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-26 14:13:14 -0300

    feat: updateTask usecase

---
+++ Tasks/application/usecases/UpdateTask.cs
@@ -0,0 +1,24 @@
+using Tasks.Domain.Repositories;
+using Tasks.Domain.Services;
+using Tasks.Application.DTOs;
+using Tasks.Application.Mappers;
+
+namespace Tasks.Application.Usecases;
+
+public class UpdateTaskUseCase(ITaskRepository taskRepository, TaskDomainService taskDomainService)
+{
+    private readonly ITaskRepository _taskRepository = taskRepository;
+    private readonly TaskDomainService _taskDomainService = taskDomainService;
+
+    public async Task<TaskResponseDTO> ExecuteAsync(Guid id, TaskRequestDTO request)
+    {
+        var task = await _taskRepository.GetByIdAsync(id)
+            ?? throw new KeyNotFoundException("Task not found.");
+
+        _taskDomainService.UpdateTask(task, request.Title, request.Description);
+
+        await _taskRepository.UpdateAsync(task);
+
+        return TaskMapper.ToDTO(task);
+    }
+}
