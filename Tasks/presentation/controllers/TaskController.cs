3b9102e9c350681eefe330589e95672f53ab2122
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-26 17:08:45 -0300

    feat: update Task controller

--- Tasks/presentation/controllers/TaskController.cs
+++ Tasks/presentation/controllers/TaskController.cs
@@ -3,25 +3,37 @@ using Microsoft.AspNetCore.Mvc;
 using Tasks.Application.Usecases;
 using Tasks.Presentation.DTOs;
 using Tasks.Presentation.Mappers;
-using Tasks.Security.Config.Roles;
 
 namespace Tasks.Presentation.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class TasksController(AddTask addTaskUseCase, GetTaskById getTaskByIdUseCase) : ControllerBase
+public class TasksController(AddTask addTaskUseCase, GetTaskById getTaskByIdUseCase, UpdateTaskUseCase updateTaskUseCase) : ControllerBase
 {
     private readonly AddTask _addTaskUseCase = addTaskUseCase;
     private readonly GetTaskById _getTaskByIdUseCase = getTaskByIdUseCase;
+    private readonly UpdateTaskUseCase _updateTaskUseCase = updateTaskUseCase;
 
     [HttpPost("add")]
     // [RolesAuthorize(nameof(TasksController), nameof(CreateTask))]
     [AllowAnonymous]
     public async Task<IActionResult> CreateTask([FromBody] TaskRequest request)
     {
-
         var appRequest = TaskPresentationMapper.ToApplication(request);
         var appResponse = await _addTaskUseCase.ExecuteAsync(appRequest);
+        var response = TaskPresentationMapper.ToApi(appResponse);
+
+        return Ok(response);
+    }
+
+    [HttpPut("update")]
+    // [RolesAuthorize(nameof(TasksController), nameof(UpdateTask))]
+    [AllowAnonymous]
+    public async Task<IActionResult> UpdateTask(Guid id, [FromBody] TaskRequest request)
+    {
+        var appRequest = TaskPresentationMapper.ToApplication(request);
+        var appResponse = await _updateTaskUseCase.ExecuteAsync(id, appRequest);
+
         var response = TaskPresentationMapper.ToApi(appResponse);
 
         return Ok(response);
