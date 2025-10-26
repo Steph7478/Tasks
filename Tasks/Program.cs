0d8170820cb2db28d5e6d78996f20d25344d101f
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-26 17:08:06 -0300

    feat: new DI usecase to program.cs

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -7,21 +7,22 @@ using System.Text.Json;
 using Tasks.Security.Config.Cors;
 using Tasks.Security.Config.Headers;
 using Tasks.Security.Config.Permissions;
+using Tasks.Domain.Services;
 
-// Env configs
+// Env config
 var envConfig = new EnvConfig();
 var options = envConfig.GetDbOptions();
 
-// Start DB + Auto Dispose();
+// DB local
 using (var context = new AppDbContext(options))
 {
     context.Database.EnsureCreated();
 }
 
-// Make builder
+// Builder
 var builder = WebApplication.CreateBuilder(args);
 
-// Simple Authorizatin
+// Authorization policies
 builder.Services.AddAuthorization(options =>
 {
     foreach (var routeRole in PermissionConfig.RouteRoles)
@@ -33,8 +34,7 @@ builder.Services.AddAuthorization(options =>
     }
 });
 
-
-// Controllers
+// Controllers + JSON config
 builder.Services.AddControllers(options =>
 {
     options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
@@ -45,20 +45,30 @@ builder.Services.AddControllers(options =>
     options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 });
 
-// DI
+// DI - DbContext + GetDbOptions from EnvConfig
 builder.Services.AddScoped(_ => new AppDbContext(options));
+
+// DI - Repo && UseCases
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<TaskDomainService>();
 builder.Services.AddScoped<AddTask>();
+builder.Services.AddScoped<UpdateTaskUseCase>();
 builder.Services.AddScoped<GetTaskById>();
 
 // Build app
 var app = builder.Build();
 
+// Middleware
 app.UseHttpsRedirection();
+app.UseCorsPolicy();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseCorsPolicy();
 app.ApplySecurityHeaders();
+
+// Map controllers
 app.MapControllers();
 
+// Run
 app.Run();
+
+//dotnet run --launch-profile "https"
