68c32025570eba2a82b82ecb9614141f92e70974
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-26 14:12:15 -0300

    refactor: current status in dtos

--- Tasks/application/DTOs/TaskRequestDTO.cs
+++ Tasks/application/DTOs/TaskRequestDTO.cs
@@ -1,7 +1,10 @@
+using Tasks.Domain.Enums;
+
 namespace Tasks.Application.DTOs;
 
 public record TaskRequestDTO
 {
     public required string Title { get; set; }
     public required string Description { get; set; }
+    public Status CurrentStatus { get; set; } = Status.Pending;
 }
--- Tasks/presentation/DTOs/TaskRequest.cs
+++ Tasks/presentation/DTOs/TaskRequest.cs
@@ -1,9 +1,13 @@
+using Tasks.Domain.Enums;
+
 namespace Tasks.Presentation.DTOs
 {
     public record TaskRequest
     {
         public required string Title { get; set; }
         public required string Description { get; set; }
+        public Status CurrentStatus { get; set; } = Status.Pending;
+
     }
 
 }
