dad59079df5f894fbe49bb0d102b65662df6a089
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-28 09:31:09 -0300

    refactor: removing null validation

--- Tasks/application/usecases/GetTaskById.cs
+++ Tasks/application/usecases/GetTaskById.cs
@@ -8,10 +8,9 @@ public class GetTaskById(ITaskRepository taskRepository)
 {
     private readonly ITaskRepository _taskRepository = taskRepository;
 
-    public async Task<TaskResponseDTO?> ExecuteAsync(Guid id)
+    public async Task<TaskResponseDTO> ExecuteAsync(Guid id)
     {
         var task = await _taskRepository.GetByIdAsync(id);
-        if (task == null) return null;
 
         return TaskMapper.ToDTO(task);
     }
