5624b625aa14b7f7fa13e5da93c419aa71119f91
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-10-28 09:30:43 -0300

    refactor: adapting delete to new repo

--- Tasks/application/usecases/DeleteTask.cs
+++ Tasks/application/usecases/DeleteTask.cs
@@ -11,7 +11,8 @@ public class DeleteTaskUseCase(ITaskRepository taskRepository, TaskDomainService
 
     public async Task ExecuteAsync(DomainTask task)
     {
-        _taskDomainService.ValidateDelete(task);
-        await _taskRepository.DeleteAsync(task);
+        var entity = await _taskRepository.GetByIdAsync(task.Id);
+        _taskDomainService.ValidateDelete(entity);
+        await _taskRepository.DeleteAsync(entity);
     }
 }
